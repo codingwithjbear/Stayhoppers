6af127991643bedffcaf6b77094e8f8adc5af797
Giries <girieshattar1@gmail.com>
2023-05-21 11:07:05 -0500

    models

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class listingInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=4, decimal_places=2)
+    sqft = models.IntegerField()
+    photo = models.ImageField(upload_to='listing_photos/')
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
